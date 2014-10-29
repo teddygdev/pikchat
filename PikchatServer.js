a289b30c7efd61153ac0adbd55dbe582aebdaa79
teddygdev <teddygdev@users.noreply.github.com>
2014-10-30 00:58:48 +0200

    some basic web version working. http and tcp are separate

--- PikchatServer.js
+++ PikchatServer.js
@@ -16,6 +16,65 @@ var random = {},
     technology = {},
     auto = {};
 
+
+///////http part
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+var sockjs = require('sockjs');
+
+var connections = [];
+
+var chat = sockjs.createServer();
+chat.on('connection', function(conn) {
+    connections.push(conn);
+    var number = connections.length;
+    conn.write("Welcome, User " + number);
+    conn.on('data', function(message) {
+        for (var ii=0; ii < connections.length; ii++) {
+            connections[ii].write("User " + number + " says: " + message);
+        }
+    });
+    conn.on('close', function() {
+        for (var ii=0; ii < connections.length; ii++) {
+            connections[ii].write("User " + number + " has disconnected");
+        }
+    });
+});
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+//app.use(express.json());
+//app.use(express.urlencoded());
+//a/p.use(express.methodOverride());
+//app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+//if ('development' == app.get('env')) {
+//  app.use(express.errorHandler());
+//}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+chat.installHandlers(server, {prefix:'/chat'});
+
+
+////////////////
 //create the server
 var server = net.createServer(function(conn) {
 
@@ -252,6 +311,6 @@ var server = net.createServer(function(conn) {
 });
 
 
-server.listen(3000, function() {
+server.listen(4000, function() {
     console.log(moment().format("MMMDD|HH:mm:ss") + " \033[96m   server listening on *:3000\033[39m");
 });
