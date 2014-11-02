8fe3539562da1249e1ca030546c1033fcd5163e3
teddygdev <teddygdev@users.noreply.github.com>
2014-11-02 21:23:40 +0200

    added a new style and colors to web interface. fix express typo error

--- PikchatServer.js
+++ PikchatServer.js
@@ -98,7 +98,7 @@ chat.on('connection', function(conn) {
 // all environments
 app.engine('html', require('ejs').renderFile);
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 //app.use(express.favicon());
 //app.use(express.logger('dev'));
@@ -108,13 +108,13 @@ app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'app')));
 
 // development only
-//if ('development' == app.get('env')) {
-//  app.use(express.errorHandler());
-//}
+if ('development' == app.get('env')) {
+  //app.use(express.errorHandler());
+}
 
 app.get('/', routes.index);
 //app.get('/partials/:name', routes.partials);  //future proof
-app.get('*', routes.index);
+//app.get('/*', routes.index);
 
 var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
